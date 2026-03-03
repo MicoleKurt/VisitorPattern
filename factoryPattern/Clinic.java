e188e41d89e316c8a0d62471f03dd9353a70288f
Micole Kurt T. Gonda <micole.gonda@gmail.com>
2026-03-03 08:53:10 +0800

    Initial commit

---
+++ factoryPattern/Clinic.java
@@ -0,0 +1,38 @@
+package factoryPattern;
+import java.util.Scanner;
+
+public class Clinic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pet pet = null;
+
+        while (true) {
+            System.out.println("\nChoose a pet:");
+            System.out.println("1. Cat");
+            System.out.println("2. Dog");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+
+            if (choice == 1) {
+                pet = new Cat(9); // Default 9 lives
+                pet.makeSound();
+                pet.play();
+                System.out.println("This cat has " + ((Cat) pet).getLives() + " lives.");
+            } else if (choice == 2) {
+                pet = new Dog("Golden Retriever"); // Example breed
+                pet.makeSound();
+                pet.play();
+                System.out.println("This dog is a " + ((Dog) pet).getBreed() + ".");
+            } else if (choice == 3) {
+                System.out.println("Exiting program...");
+                break;
+            } else {
+                System.out.println("Invalid choice. Try again.");
+            }
+        }
+
+        sc.close();
+    }
+}
+
---
+++ factoryPattern/Dog.java
@@ -0,0 +1,22 @@
+package factoryPattern;
+public class Dog implements Pet {
+    private String breed;
+
+    public Dog(String breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("The dog is fetching the ball.");
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+}
